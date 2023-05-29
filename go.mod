02e520268c3f8ead78c7aca7574f8c136cc87a10
Stephen <stephengunn@gmail.com>
2023-05-29 14:49:14 -0500

    refactor with go routines

--- go.mod
+++ go.mod
@@ -3,3 +3,13 @@ module github.com/StephenGunn/midi-controller
 go 1.20
 
 require github.com/rakyll/portmidi v0.0.0-20201020180702-d436ceaa537a
+
+require (
+	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20230218063734-2c98d96c9244 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli v1.22.13 // indirect
+)
--- main.go
+++ main.go
@@ -3,81 +3,102 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/rakyll/portmidi"
 )
 
 func main() {
-	// MIDI initialization
 	err := portmidi.Initialize()
 	if err != nil {
-		log.Fatal("Error initializing PortMidi: ", err)
+		log.Fatal("Failed to initialize PortMidi:", err)
 	}
 	defer portmidi.Terminate()
 
-	// list the devices with "aplaymidi -l" to find the correct device ID
-	// leave the space in the name if you want it to work
-	deviceID := findInputDevice("MIX5R Pro ")
+	// Find the MIDI device with the name "MIX5R Pro"
+	deviceID := findMIDIDevice("MIX5R Pro ")
 	if deviceID == -1 {
-		log.Fatal("MIDI device not found")
+		log.Fatal("MIDI device not found: MIX5R Pro")
 	}
 
-	// connect to our target device
-	input, err := portmidi.NewInputStream(portmidi.DeviceID(deviceID), 1024)
+	// Open the MIDI input stream for the specified device
+	stream, err := portmidi.NewInputStream(portmidi.DeviceID(deviceID), 1024)
 	if err != nil {
-		log.Fatal("Error connecting to MIDI input device:", err)
+		log.Fatalf("Failed to open MIDI input stream: %v", err)
 	}
-	defer input.Close()
+	defer stream.Close()
 
-	// HANDLE MIDI INPUT
-	fmt.Println("Reading MIDI input from device:", deviceID)
-	ch := input.Listen()
+	// Set up signal handling to gracefully exit the program
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		fmt.Println("Exiting...")
+		os.Exit(0)
+	}()
 
-	fmt.Println(ch)
-
+	fmt.Println("Listening to MIDI input...")
 	for {
-		event := <-ch
+		// Read available MIDI events from the input stream
+		events, err := stream.Read(1024)
+		if err != nil {
+			log.Printf("Failed to read MIDI events: %v", err)
+			continue
+		}
 
-		// Process MIDI event
-		fmt.Println("Received MIDI event:", event)
-		status := event.Status // check against value 176 for proper midi function
-		channel := event.Data1
-		value := event.Data2
+		// Process each MIDI event
+		for _, event := range events {
+			// Print the MIDI event information
+			fmt.Printf("Received MIDI event: Status=%d, Data1=%d, Data2=%d\n",
+				event.Status, event.Data1, event.Data2)
 
-		// handle the speaker volume changes
-		if status == 176 && channel == 11 {
-			if value > 100 {
-				setVolume(100)
-			} else {
-				setVolume(value)
+			// Process MIDI event
+			status := event.Status // check against value 176 for proper midi function
+			channel := event.Data1
+			value := event.Data2
+
+			// handle the speaker volume changes
+			if status == 176 && channel == 11 {
+				if value > 100 {
+					setVolume(100)
+				} else {
+					setVolume(value)
+				}
+			}
+
+			// handle the mic gain changes
+			if status == 176 && channel == 1 {
+				if value > 100 {
+					setGain(100)
+				} else {
+					setGain(value)
+				}
 			}
 		}
 
-		// handle the mic gain changes
-		if status == 176 && channel == 1 {
-			if value > 100 {
-				setGain(100)
-			} else {
-				setGain(value)
-			}
-		}
+		// Sleep for a short duration to reduce CPU usage
+		time.Sleep(10 * time.Millisecond)
 	}
 }
 
-func findInputDevice(deviceName string) int {
+func findMIDIDevice(deviceName string) int {
 	numDevices := portmidi.CountDevices()
-	fmt.Println("-- all available MIDI devices --")
 	for i := 0; i < numDevices; i++ {
-		// let's print out each midi device for fun
 		info := portmidi.Info(portmidi.DeviceID(i))
 
-		// newb logging
-		fmt.Println("DEVICE:", info.Name, ", available:", info.IsInputAvailable)
+		// escape early
+		if info == nil {
+			return -1
+		}
 
-		if info.IsInputAvailable && info.Name == deviceName {
+		// make sure we have the correct midi device
+		if info.Name == deviceName && info.IsInputAvailable {
 			return i
 		}
 	}
