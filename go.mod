10f7f406a82d5c5c1ff68012885130a71a79ed95
Stephen <stephengunn@gmail.com>
2023-05-29 13:32:52 -0500

    working program

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/StephenGunn/midi-controller
 
 go 1.20
+
+require (
+	github.com/godbus/dbus v4.1.0+incompatible
+	github.com/rakyll/portmidi v0.0.0-20201020180702-d436ceaa537a
+)
--- main.go
+++ main.go
@@ -1,7 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/rakyll/portmidi"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// MIDI initialization
+	err := portmidi.Initialize()
+	if err != nil {
+		log.Fatal("Error initializing PortMidi: ", err)
+	}
+	defer portmidi.Terminate()
+
+	// list the devices with "aplaymidi -l" to find the correct device ID
+	deviceID := findInputDevice("MIX5R Pro ") // leave the space
+	if deviceID == -1 {
+		log.Fatal("MIDI device not found")
+	}
+
+	// connect to our target device
+	input, err := portmidi.NewInputStream(portmidi.DeviceID(deviceID), 1024)
+	if err != nil {
+		log.Fatal("Error connecting to MIDI input device:", err)
+	}
+	defer input.Close()
+
+	log.Println("Speaker volume set successfully.")
+
+	// HANDLE MIDI INPUT
+	fmt.Println("Reading MIDI input from device:", deviceID)
+	ch := input.Listen()
+	for {
+		event := <-ch
+		// Process MIDI event
+		fmt.Println("Received MIDI event:", event)
+		status := event.Status // check against value 176 for proper midi function
+		channel := event.Data1
+		value := event.Data2
+
+		// handle the speaker volume changes
+		if status == 176 && channel == 11 {
+			if value > 100 {
+				setVolume(100)
+			} else {
+				setVolume(value)
+			}
+		}
+
+		// handle the mic gain changes
+		if status == 176 && channel == 1 {
+			if value > 100 {
+				setGain(100)
+			} else {
+				setGain(value)
+			}
+		}
+	}
+}
+
+func findInputDevice(deviceName string) int {
+	numDevices := portmidi.CountDevices()
+	fmt.Println("-- all available MIDI devices --")
+	for i := 0; i < numDevices; i++ {
+		// let's print out each midi device for fun
+		info := portmidi.Info(portmidi.DeviceID(i))
+
+		// newb logging
+
+		fmt.Println("DEVICE:", info.Name, ", available:", info.IsInputAvailable)
+
+		if info.IsInputAvailable && info.Name == deviceName {
+			return i
+		}
+	}
+	return -1
+}
+
+func setVolume(volPercent int64) {
+	// execute the pactl command
+	// Execute the pactl command to set the volume
+	cmd := exec.Command("pactl", "set-sink-volume", "@DEFAULT_SINK@", strings.Join([]string{strconv.Itoa(int(volPercent)), "%"}, ""))
+	commandErr := cmd.Run()
+	if commandErr != nil {
+		log.Fatal("Failed to set speaker volume:", commandErr)
+	}
+
+	log.Println("Speaker volume set successfully.")
+}
+
+func setGain(gainPercent int64) {
+	// execute the pactl command
+	// Execute the pactl command to set the volume
+	cmd := exec.Command("pactl", "set-source-volume", "@DEFAULT_SOURCE@", strings.Join([]string{strconv.Itoa(int(gainPercent)), "%"}, ""))
+	commandErr := cmd.Run()
+	if commandErr != nil {
+		log.Fatal("Failed to set mic volume:", commandErr)
+	}
+
+	log.Println("Mic volume set successfully.")
 }
